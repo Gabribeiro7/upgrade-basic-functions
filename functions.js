38fcb64dca482e2e5d268ba01d7b4f64dbbc1cb1
Gabribeiro7 <gabriel.cardoso@bootcamp-upgrade.com>
2023-01-12 20:51:37 +0100

    Interación 1, 2 y 3

---
+++ functions.js
@@ -0,0 +1,58 @@
+// ? Iteración 1
+//? Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  } else {
+    return numberTwo;
+  }
+}
+
+//*? **Iteración #2: Buscar la palabra más larga**
+//*? Completa la función que tomando un array de strings como argumento devuelva el más largo,
+//*? en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+let avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(param) {
+  let longestWord = param[0]; 
+  
+  
+  for (let index = 0; index < param.length; index++) {
+    let element = param[index];
+    
+    if (element.length > longestWord.length) {
+      
+      longestWord = element;
+    }
+  }
+  return longestWord;
+}
+console.log(findLongestWord(avengers));
+
+
+// **Iteración #3: Calcular la suma**
+
+// Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+// Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve 
+// la suma de todos los números de la matriz.
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  let sumNumbers = 0;
+
+  for (let i = 0; i < param.length; i++) {
+    
+  sumNumbers += param[i];
+  }
+  return sumNumbers;
+}
+console.log(sumAll(numbers));
