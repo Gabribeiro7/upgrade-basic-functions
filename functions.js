085ccda8de1047533ea79d68b2a5880e30f3105a
Gabribeiro7 <gabriel.cardoso@bootcamp-upgrade.com>
2023-01-13 21:07:58 +0100

    Iteración 7

--- functions.js
+++ functions.js
@@ -136,3 +136,34 @@ function removeDuplicates(array) {
   return console.log(array);
 }
 removeDuplicates(duplicates);
+
+
+//*?**Iteración #7: Buscador de nombres**
+
+//*?Crea una función que reciba por parámetro un array y el valor que desea comprobar que 
+//*?existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos 
+//*?devuelve la posición de dicho elemento y por la contra un false
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function finderName(array , doesItExist) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]=== doesItExist){
+      return console.log(true, i);
+    }
+    
+  }
+  return console.log(false);
+}
+finderName(nameFinder, "Xabier");
